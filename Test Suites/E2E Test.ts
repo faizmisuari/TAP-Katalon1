55ece24be716f271f06453d4598c83ebfc1bb210
Admin <Admin@DESKTOP-R723454>
2024-02-03 00:18:20 +0800

    !st structure update

---
+++ Test Suites/E2E Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>E2E Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fa2fe07b-f1ab-4f72-b0f3-ad8221f96dea</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Unit Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Web GUI Test CURA Healthcare</description>
+   <name>Unit Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1e157fb2-63e7-4c3c-947e-a2d9ad7c545f</testSuiteGuid>
+</TestSuiteEntity>
